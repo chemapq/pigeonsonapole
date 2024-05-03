ca5fa1600c4cf8e577491587b1b62f9fc690261b
Chema <chpiquero@gmail.com>
2024-05-03 20:32:39 +0200

    added react

--- README.md
+++ README.md
@@ -10,3 +10,35 @@ after a while pigeon flies away
 
 if theres some chatters active at the same time they occupy horizontal space of electricity line
 
+
+-----------
+
+
+App ->
+
+The pole line covering the whole width
+
+when new message comes
+
+-if its a new chatter
+
+create a new bird
+
+display message in bubble box
+
+set a timer of 1 minute
+
+
+-if its an old chatter
+
+push previous bubble box up
+
+display message in bubble box
+
+set new timer for 1min
+
+-when timer runs out
+
+bird leaves
+
+chatter gets removed from the list
--- index.js
+++ index.js
@@ -1,138 +1,32 @@
-#!/usr/bin/env node
-const WebSocketClient = require('websocket').client;
+import tmi from 'tmi.js'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App'
+import reportWebVitals from './reportWebVitals';
 
-const client = new WebSocketClient();
-const channel = '#<channel>';  // Replace with your channel.
-const account = '<account>';   // Replace with the account the bot runs as
-const password = 'oauth:<your access token goes here>';
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
 
-const moveMessage = 'Get up and move, your body will thank you!';
-const defaultMoveInterval = 1000 * 60 * 1; // Set to 1 minute for testing.
-let moveInterval = defaultMoveInterval;
 
-client.on('connectFailed', function(error) {
-    console.log('Connect Error: ' + error.toString());
+
+const client = new tmi.Client({
+  connection: {
+    secure: true,
+    reconnect: true
+  },
+  channels: [ 'poponut' ]
 });
 
-client.on('connect', function(connection) {
-    console.log('WebSocket Client Connected');
+client.connect();
 
-    // This is a simple bot that doesn't need the additional
-    // Twitch IRC capabilities.
+client.on('message', (channel, tags, message, self) => {
+  console.log(`${tags['display-name']}: ${message}`);
 
-    // connection.sendUTF('CAP REQ :twitch.tv/commands twitch.tv/membership twitch.tv/tags');
-    
-    // Authenticate with the Twitch IRC server and then join the channel.
-    // If the authentication fails, the server drops the connection.
 
-    connection.sendUTF(`PASS ${password}`); 
-    connection.sendUTF(`NICK ${account}`);
-
-    // Set a timer to post future 'move' messages. This timer can be
-    // reset if the user passes, !move [minutes], in chat.
-    let intervalObj = setInterval(() => {
-        connection.sendUTF(`PRIVMSG ${channel} :${moveMessage}`);
-    }, moveInterval);
-    
-    connection.on('error', function(error) {
-        console.log("Connection Error: " + error.toString());
-    });
-
-    connection.on('close', function() {
-        console.log('Connection Closed');
-        console.log(`close description: ${connection.closeDescription}`);
-        console.log(`close reason code: ${connection.closeReasonCode}`);
-
-        clearInterval(intervalObj);
-    });
-
-    // Process the Twitch IRC message.
-
-    connection.on('message', function(ircMessage) {
-        if (ircMessage.type === 'utf8') {
-            let rawIrcMessage = ircMessage.utf8Data.trimEnd();
-            console.log(`Message received (${new Date().toISOString()}): '${rawIrcMessage}'\n`);
-
-            let messages = rawIrcMessage.split('\r\n');  // The IRC message may contain one or more messages.
-            messages.forEach(message => {
-                let parsedMessage = parseMessage(message);
-            
-                if (parsedMessage) {
-                    // console.log(`Message command: ${parsedMessage.command.command}`);
-                    // console.log(`\n${JSON.stringify(parsedMessage, null, 3)}`)
-
-                    switch (parsedMessage.command.command) {
-                        case 'PRIVMSG':
-                            // Ignore all messages except the '!move' bot
-                            // command. A user can post a !move command to change the 
-                            // interval for when the bot posts its move message.
-
-                            if ('move' === parsedMessage.command.botCommand) {
-                                
-                                // Assumes the command's parameter is well formed (e.g., !move 15).
-
-                                let updateInterval = (parsedMessage.command.botCommandParams) ?
-                                    parseInt(parsedMessage.command.botCommandParams) * 1000 * 60 : defaultMoveInterval;
-
-                                if (moveInterval != updateInterval) {
-                                    // Valid range: 1 minute to 60 minutes
-                                    if (updateInterval >= 60000 && updateInterval <= 3600000) {  
-                                        moveInterval = updateInterval;
-
-                                        // Reset the timer.
-                                        clearInterval(intervalObj);
-                                        intervalObj = null;
-                                        intervalObj = setInterval(() => {
-                                            connection.sendUTF(`PRIVMSG ${channel} :${moveMessage}`);
-                                        }, moveInterval);
-                                    }
-                                }
-                            }
-                            else if ('moveoff' === parsedMessage.command.botCommand) {
-                                clearInterval(intervalObj);
-                                connection.sendUTF(`PART ${channel}`);
-                                connection.close();
-                            }
-                            
-                            break;
-                        case 'PING':
-                            connection.sendUTF('PONG ' + parsedMessage.parameters);
-                            break;
-                        case '001':
-                            // Successfully logged in, so join the channel.
-                            connection.sendUTF(`JOIN ${channel}`); 
-                            break; 
-                        case 'JOIN':
-                            // Send the initial move message. All other move messages are
-                            // sent by the timer.
-                            connection.sendUTF(`PRIVMSG ${channel} :${moveMessage}`);
-                            break;
-                        case 'PART':
-                            console.log('The channel must have banned (/ban) the bot.');
-                            connection.close();
-                            break;
-                        case 'NOTICE': 
-                            // If the authentication failed, leave the channel.
-                            // The server will close the connection.
-                            if ('Login authentication failed' === parsedMessage.parameters) {
-                                console.log(`Authentication failed; left ${channel}`);
-                                connection.sendUTF(`PART ${channel}`);
-                            }
-                            else if ('You don’t have permission to perform that action' === parsedMessage.parameters) {
-                                console.log(`No permission. Check if the access token is still valid. Left ${channel}`);
-                                connection.sendUTF(`PART ${channel}`);
-                            }
-                            break;
-                        default:
-                            ; // Ignore all other IRC messages.
-                    }
-                }
-            });
-        }
-    });
-    
 });
-
-client.connect('ws://irc-ws.chat.twitch.tv:80');
-
 
